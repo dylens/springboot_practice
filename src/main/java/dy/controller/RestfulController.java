bc7f5ce93a915287ab417188663b3f67d2bdc147
dylens <ylsync@qq.com>
2020-02-06 12:37:23 +0800

    ..

--- src/main/java/dy/controller/RestfulController.java
+++ src/main/java/dy/controller/RestfulController.java
@@ -6,6 +6,7 @@ import dy.dto.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -19,7 +20,7 @@ public class RestfulController {
         return "home";
     }
 
-    @RequestMapping("/api/student")
+    @RequestMapping(value = "/api/student",method = RequestMethod.GET)
     @ResponseBody
     public String student() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
